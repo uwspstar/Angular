6a478b7e4dc9a3754874d680e65ef55747fd687b
xing wang <uwspstar@gmail.com>
2017-12-19 22:20:38 -0600

    no message

--- demoMT/src/app/app-routing.module.ts
+++ demoMT/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './ui/home/home.component';
+import { LoginComponent } from './ui/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- demoMT/src/app/app.module.ts
+++ demoMT/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialsModule } from './materials/materials.module';
+import { HomeComponent } from './ui/home/home.component';
+import { LoginComponent } from './ui/login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
